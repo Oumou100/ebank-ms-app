91d3407a0749699b51a8b6ecd8731d3aa9f410b0
Oumou100 <dilymalidi10@gmail.com>
2026-09-13 15:10:01 +0100

    feat(gateway): dynamic routing

--- gateway-service/src/main/java/net/oumoudev/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/net/oumoudev/gatewayservice/GatewayServiceApplication.java
@@ -2,6 +2,10 @@ package net.oumoudev.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.composite.reactive.ReactiveCompositeDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
@@ -10,4 +14,8 @@ public class GatewayServiceApplication {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
+    @Bean
+    DiscoveryClientRouteDefinitionLocator routes(ReactiveCompositeDiscoveryClient rdc, DiscoveryLocatorProperties tp){
+        return new DiscoveryClientRouteDefinitionLocator(rdc,tp);
+    }
 }
