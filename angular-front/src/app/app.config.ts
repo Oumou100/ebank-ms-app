d6aa222352fbad4685e8919f30c2f6b7b58b97bd
Oumou100 <dilymalidi10@gmail.com>
2026-09-18 20:27:59 +0100

    feat(bot-ui): integrate markdown support and loading indicator in bot UI

--- angular-front/src/app/app.config.ts
+++ angular-front/src/app/app.config.ts
@@ -1,12 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { provideMarkdown } from 'ngx-markdown';
+import { loadingInterceptorInterceptor } from './interceptors/loading-interceptor-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(withInterceptors([loadingInterceptorInterceptor])),
+    provideMarkdown(),
   ]
 };
--- angular-front/src/app/bot-ui/bot-ui.ts
+++ angular-front/src/app/bot-ui/bot-ui.ts
@@ -1,9 +1,13 @@
 import { HttpClient } from '@angular/common/http';
+import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MarkdownComponent } from 'ngx-markdown';
 import { Observable } from 'rxjs';
+import { LoadingService } from '../services/loading';
 
 @Component({
-  imports: [],
+  imports: [AsyncPipe, FormsModule, MarkdownComponent],
   selector: 'app-bot-ui',
   styleUrl: './bot-ui.css',
   templateUrl: './bot-ui.html',
@@ -12,9 +16,10 @@ export class BotUi {
   query: any;
   http = inject(HttpClient);
   response$! : Observable<any>
+  public loadingService  = inject(LoadingService);
 
   askAgent() {
-    this.response$ = this.http.get("http://localhost:9999/EBANK-BOT/bot?query=" + this.query,
+    this.response$ = this.http.get("http://localhost:9999/EBANK-BOT/chat?query=" + this.query,
        { responseType: 'text' });
   }
 }
