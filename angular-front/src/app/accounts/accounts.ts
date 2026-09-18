740aa958d359b2ab877bfc5f1be8dc52a9f67dcb
Oumou100 <dilymalidi10@gmail.com>
2026-09-18 13:37:29 +0100

    feat(loading): implement loading service and interceptor for loading state management

--- angular-front/src/app/accounts/accounts.ts
+++ angular-front/src/app/accounts/accounts.ts
@@ -3,6 +3,7 @@ import { Component, inject } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 import { catchError, map, Observable, of } from 'rxjs';
 import { Account, AccountListState, RequestStatus } from './model/account.model';
+import { LoadingService } from '../services/loading';
 
 @Component({
   imports: [AsyncPipe],
@@ -14,6 +15,7 @@ export class Accounts {
   readonly RequestStatus = RequestStatus;
   private http = inject(HttpClient);
   // constructor(private http: HttpClient) {} injection via constructeur necessite d'être dans une classe alors  que parfois on est dans une fonction, on est pas forcement dans une classe donc la solution restant est inject
+  public loadService  = inject(LoadingService);
   accounts$ : Observable<AccountListState>  = this.http.get<Account[]>('http://localhost:9999/EBANK-SERVICE/accounts')
   .pipe(
     map(resp => {
